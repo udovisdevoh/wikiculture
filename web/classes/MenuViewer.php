baa5876b9556f5fee6e3e4eb7419938bc0b29e93
glacasse <glacasse@b9b078ea-8ec5-5f69-6ab5-5028d69ea090>
2010-03-25 18:01:47 +0000

    Pages

---
+++ web/classes/MenuViewer.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once("AssemblyInfo.ns.php");
+
+class MenuViewer
+{
+	//$wiki: Wiki
+	//retourne le code HTML du menu d'un wiki
+	public static getHtmlCode($wiki)
+	{
+		//TODO
+	}
+}
+
+?>
