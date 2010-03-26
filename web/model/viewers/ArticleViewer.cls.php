b8c31801b3306a265c73494490b0959944b9fb3e
glacasse <glacasse@b9b078ea-8ec5-5f69-6ab5-5028d69ea090>
2010-03-26 15:13:31 +0000

    Modifié style

--- web/model/viewers/ArticleViewer.cls.php
+++ web/model/viewers/ArticleViewer.cls.php
@@ -4,11 +4,15 @@ class ArticleViewer
 {
 	//$article: objet de type article
 	//Retourne le code HTML d'un article
-	public static function getHtmlCode($article)
+	public static function getHtmlCode($wiki, $article)
 	{
-		echo '<pre>';
-		print_r($article);
-		echo '</pre>';
+		$html .= '<div class="Article">';
+	
+			$html .= '<h1>'.$wiki->getTitle().' : '.$article->getTitle().'</h1>';	
+			$html .= $article->getContent();
+		
+		$html .= '</div>';		
+		return $html;
 	}
 }
 
--- web/model/viewers/MenuViewer.cls.php
+++ web/model/viewers/MenuViewer.cls.php
@@ -10,17 +10,19 @@ class MenuViewer
 	
 		$html = "";
 		
-		$html .= "<ul>";
-			$html .= "<li>";
-				$html .= '<a href="./?wiki_title='.urlencode($wiki->getTitle()).'">'.$language->menu->randomPage.'</a>';
-			$html .= "</li>";
-			$html .= "<li>";
-				$html .= '<a href="contactUs.php?wiki_title='.urlencode($wiki->getTitle()).'">'.$language->menu->contactUs->name.'</a>';
-			$html .= "</li>";
-			$html .= "<li>";
-				$html .= SearchFormViewer::getHtmlCode($wiki);
-			$html .= "</li>";
-		$html .= "</ul>";
+		$html .= '<div class="LeftMenu">';
+			$html .= "<ul>";
+				$html .= "<li>";
+					$html .= '<a href="./?wiki_title='.urlencode($wiki->getTitle()).'">'.$language->menu->randomPage.'</a>';
+				$html .= "</li>";
+				$html .= "<li>";
+					$html .= '<a href="contactUs.php?wiki_title='.urlencode($wiki->getTitle()).'">'.$language->menu->contactUs->name.'</a>';
+				$html .= "</li>";
+				$html .= "<li>";
+					$html .= SearchFormViewer::getHtmlCode($wiki);
+				$html .= "</li>";
+			$html .= "</ul>";
+		$html .= '</div>';
 		
 		return $html;
 	}
