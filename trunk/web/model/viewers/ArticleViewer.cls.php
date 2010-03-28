6d6d1320a160eecc206b253545331858d20c32cc
glacasse <glacasse@b9b078ea-8ec5-5f69-6ab5-5028d69ea090>
2010-03-28 15:47:19 +0000

    Ajouté stub de parser de tag de lien

--- trunk/web/model/viewers/ArticleViewer.cls.php
+++ trunk/web/model/viewers/ArticleViewer.cls.php
@@ -9,10 +9,18 @@ class ArticleViewer
 		$html .= '<div class="Article">';
 	
 			$html .= '<h1>'.$wiki->getTitle().' : '.$article->getTitle().'</h1>';	
-			$html .= $article->getContent();
+			$html .= ArticleViewer::parseTags($article->getContent());
 		
 		$html .= '</div>';		
 		return $html;
+	}
+	
+	//Retourne le texte avec certains tag parsés, par exemple:
+	//Lien vers un article: [nom de l'article]
+	//Lien vers un article sur une autre wiki: [nom de la wiki|nom de l'article]
+	public static function parseTags($rawText)
+	{
+		return $rawText;
 	}
 }
 
