378d147267471a3109aba7e92fb003975f0313e1
glacasse <glacasse@b9b078ea-8ec5-5f69-6ab5-5028d69ea090>
2010-03-28 15:14:55 +0000

    Added dummy page

---
+++ web/dummyIndex.php
@@ -0,0 +1,23 @@
+﻿<?php
+	require_once('model/AssemblyInfo.ns.php');
+	
+	$wiki = new Wiki();
+	$article = new Article();
+?>		
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//<?php echo $wiki->getLanguageName()?>" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $wiki->getLanguageName()?>" lang="<?php echo $wiki->getLanguageName()?>">
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta name="language" content="<?php echo $wiki->getLanguageName()?>">
+		<link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<title>
+			<?php echo $wiki->getTitle()." : ".$article->getTitle(); ?>
+		</title>
+	</head>
+	<body>
+		<?php
+			echo MenuViewer::getHtmlCode($wiki, $article);
+			echo ArticleViewer::getHtmlCode($wiki, $article);
+		?>
+	</body>
+</html>
--- web/model/objectClasses/Wiki.cls.php
+++ web/model/objectClasses/Wiki.cls.php
@@ -6,14 +6,14 @@ class Wiki
 	
 	private $title; //Titre de la wiki également sous forme de clef unique
 	
-	private $ownerList; //Liste des propriétaires de la wiki sous forme de liste de email
+	private $ownerList; //Liste des propriétaires de la wiki sous forme de liste de email dans une string
 		
 	private $languageName; //Nom de fichier XML de langue (sans extension XML)
 	
 	public function __construct()
 	{
 		$this->title = "Wiki sans-titre";
-		$this->ownerList = array();
+		$this->ownerList = "";
 		$this->languageName = "fr";
 	}
 	
