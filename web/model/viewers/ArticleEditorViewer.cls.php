23d3f29f125d152e3d1fcf62c4bead72a99f3e55
glacasse <glacasse@b9b078ea-8ec5-5f69-6ab5-5028d69ea090>
2010-03-28 22:42:50 +0000

    Intégré CKEditor (éditeur de rich text)

--- web/model/viewers/ArticleEditorViewer.cls.php
+++ web/model/viewers/ArticleEditorViewer.cls.php
@@ -11,10 +11,15 @@ class ArticleEditorViewer
 		$html .= '<div class="ArticleEditor">';
 			$html .= '<h1>'.$wiki->getTitle().' : '.$article->getTitle().'</h1>';	
 			$html .= '<form method="post" action="'.$_SERVER["REQUEST_URI"].'">';
-				$html .= '<textarea cols="100" rows="15" name="article_content">'.$article->getContent().'</textarea>';
+				$html .= '<textarea cols="100" rows="35" name="article_content">'.$article->getContent().'</textarea>';
 				$html .= '<div><input type="submit" name="submit" value="'.$language->article->save.'" /></div>';
 			$html .= '<form>';
 		$html .= '</div>';		
+		
+		$html .= "<script type=\"text/javascript\">\n";
+		$html .= "CKEDITOR.replace( 'article_content' );\n";
+		$html .= "</script>\n";
+		
 		return $html;
 	}
 }
