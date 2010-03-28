72860b07d8edaf5d774ea4e66a7b0bedf25e0e0d
glacasse <glacasse@b9b078ea-8ec5-5f69-6ab5-5028d69ea090>
2010-03-28 16:08:20 +0000

    Parsing de tag de liens d'article: fonctionnel

--- web/model/viewers/ArticleViewer.cls.php
+++ web/model/viewers/ArticleViewer.cls.php
@@ -8,8 +8,8 @@ class ArticleViewer
 	{
 		$html .= '<div class="Article">';
 	
-			$html .= '<h1>'.$wiki->getTitle().' : '.$article->getTitle().'</h1>';	
-			$html .= ArticleViewer::parseTags($article->getContent());
+			$html .= '<h1>'.$wiki->getTitle().' | '.$article->getTitle().'</h1>';	
+			$html .= ArticleViewer::parseTags($article->getContent(),$wiki);
 		
 		$html .= '</div>';		
 		return $html;
@@ -18,9 +18,54 @@ class ArticleViewer
 	//Retourne le texte avec certains tag parsés, par exemple:
 	//Lien vers un article: [nom de l'article]
 	//Lien vers un article sur une autre wiki: [nom de la wiki|nom de l'article]
-	public static function parseTags($rawText)
+	public static function parseTags($rawText,$wiki)
 	{
-		return $rawText;
+		//$rawText = str_replace("[","[(tagStart)",$rawText);
+		$rawText = str_replace("]","(tagEnd)]",$rawText);
+		
+		$newText = "";
+		
+		$chunkList = explode("[",$rawText);
+		
+		foreach ($chunkList as $chunk)
+		{
+			if (strstr($chunk,"]"))
+			{
+				$lineList = explode("]",$chunk);
+				foreach ($lineList as $line)
+				{
+					if (strstr($line,"(tagEnd)"))
+					{
+						$newText .= ArticleViewer::parseTag(str_replace("(tagEnd)","",$line),$wiki);
+					}
+					else
+					{
+						$newText .= $line;
+					}
+				}
+			}
+			else
+			{
+				$newText .= $chunk;
+			}
+		}
+		
+		return $newText;
+	}
+	
+	public static function parseTag($line,$wiki)
+	{
+		if (strstr($line,"|"))
+		{
+			$subTags = explode("|",$line);
+			$wikiTitle = $subTags[0];
+			$articleTitle = $subTags[1];
+			return '<a href="./?wiki_title='.urlencode($wikiTitle).'&article_title='.urlencode($articleTitle).'">'.$articleTitle.'</a>';
+		}
+		else
+		{
+			return '<a href="./?wiki_title='.urlencode($wiki->getTitle()).'&article_title='.urlencode($line).'">'.$line.'</a>';
+		}
 	}
 }
 
