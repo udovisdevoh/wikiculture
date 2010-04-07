d7bd89404d421b5b605836e2816a44da7d8644cb
glacasse <glacasse@b9b078ea-8ec5-5f69-6ab5-5028d69ea090>
2010-04-07 21:12:55 +0000

    envoie d'email: works

--- web/model/managers/EmailManager.cls.php
+++ web/model/managers/EmailManager.cls.php
@@ -2,4 +2,22 @@
 
 class EmailManager
 {
+	public static function sendEmail($wiki, $senderEmail, $message)
+	{
+		$ownerList = $wiki->getOwnerList();
+		
+		$ownerList = str_replace(" ","\n",$ownerList);
+		$ownerList = str_replace("\r","\n",$ownerList);
+		$ownerList = str_replace("\t","\n",$ownerList);
+		$ownerList = str_replace(",","\n",$ownerList);
+		
+		while (strstr($ownerList,"\n\n"))
+			$this->ownerList = str_replace("\n\n", "\n",$ownerList);
+		
+		$ownerList = explode("\n",$ownerList);
+
+		foreach ($ownerList as $ownerEmail)
+			if (strlen($ownerList) >0)
+				mail($ownerEmail, "WikiCulture", $message);
+	}
 }
